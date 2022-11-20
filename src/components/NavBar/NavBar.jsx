ebfed0651230cd8271d50d1d4b1458b7093e75d4
EricWithC04 <negativegame@gmail.com>
2022-11-20 20:19:08 -0300

    navBar complete

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from "react-icons/fa"
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
     return (
         <div className={styles.header}>
             <Link to="/">
                 <h1>Portfolio</h1>
             </Link>
-            <ul className={styles.navMenu}>
+            <ul className={click ? styles.navMenuActive : styles.navMenu}>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
@@ -18,7 +24,19 @@ const NavBar = () => {
                 <li>
                     <Link to="/About">About</Link>
                 </li>
+                <li>
+                    <Link to="/About">Contact</Link>
+                </li>
             </ul>
+            <div className={styles.hamburger} onClick={(e) => handleClick()}>
+                {
+                    click ? (
+                        <FaTimes size={20} style={{ color: "#fff" }}/>
+                    ) : (
+                        <FaBars size={20} style={{ color: "#fff" }}/>
+                    )
+                }   
+            </div>
         </div>
     );
 };
